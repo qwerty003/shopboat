ab1fbd1131c50c3b390b89e47a28bb6801429b6f
qwerty003 <abhijeetcatches33@gmail.com>
2024-09-12 21:11:28 +0530

    Initial Commit

---
+++ Shopboat/ShopboatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopboatApp.swift
+//  Shopboat
+//
+//  Created by Abhijeet Kumar  on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShopboatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
